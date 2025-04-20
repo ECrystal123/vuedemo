4fb99f9c88be282efae25cd1a7c7487520a0423f
ECrystal123 <zhz20121211@126.com>
2025-04-20 22:55:21 +0800

    update

--- vuedemo/vite.config.js
+++ vuedemo/vite.config.js
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/vuedemo/', // 重点：替换为你的仓库名（前后有斜杠）
   plugins: [
     vue(),
     vueDevTools(),
